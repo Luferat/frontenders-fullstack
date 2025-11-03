607654d5b6d6e80fc69855275f75a8a0d50264af
André Luferat <contato@luferat.net>
2025-11-03 12:49:34 -0300

    Templates ok

--- static/js/firebaseConfig.js
+++ static/js/firebaseConfig.js
@@ -1,1 +1,8 @@
-/** VAZIO por enquanto */
+const firebaseConfig = {
+  apiKey: "AIzaSyBHC6fxBShUXjcdjBFjJ8k1bF34HKKVP3c",
+  authDomain: "frontenders-1d1c1.firebaseapp.com",
+  projectId: "frontenders-1d1c1",
+  storageBucket: "frontenders-1d1c1.firebasestorage.app",
+  messagingSenderId: "991944134559",
+  appId: "1:991944134559:web:df606bb7575a07489e77aa"
+};
